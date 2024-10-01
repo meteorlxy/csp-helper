cd78c59e79f8954c587fd60cf99fdfbba7797eb9
meteorlxy <meteor.lxy@foxmail.com>
2024-10-01 23:00:50 +0800

    fix: add connect-src and img-src for vimeo

--- src/presets/vimeo.ts
+++ src/presets/vimeo.ts
@@ -6,6 +6,8 @@ import type { ContentSecurityPolicyConfig } from '../types';
  * @see https://github.com/vimeo/player.js?tab=readme-ov-file#readme
  */
 export const CSP_PRESET_VIMEO_EMBED = {
+  'connect-src': `https://vimeo.com`,
   'frame-src': `https://player.vimeo.com`,
+  'img-src': `https://i.vimeocdn.com`,
   'script-src': `https://player.vimeo.com`,
 } satisfies ContentSecurityPolicyConfig;
