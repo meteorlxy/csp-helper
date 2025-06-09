1dbcf3562b8aae37574e75e8c5e744ef1c10dc81
meteorlxy <meteor.lxy@foxmail.com>
2025-06-09 18:29:13 +0800

    fix: update youtube image cdn url

--- src/presets/youtube.ts
+++ src/presets/youtube.ts
@@ -5,5 +5,5 @@ import type { ContentSecurityPolicyConfig } from '../types';
  */
 export const CSP_PRESET_YOUTUBE_EMBED = {
   'frame-src': `https://www.youtube.com https://www.youtube-nocookie.com`,
-  'img-src': `https://*.ytimg.com`,
+  'img-src': `https://i.ytimg.com`,
 } satisfies ContentSecurityPolicyConfig;
