32b773b2db2fd1cb122f7ba72becd007f232af7e
meteorlxy <meteor.lxy@foxmail.com>
2026-01-15 18:06:08 +0800

    feat: add tiktok pixel preset

--- src/presets/tiktok.ts
+++ src/presets/tiktok.ts
@@ -7,3 +7,15 @@ export const CSP_PRESET_TIKTOK_EMBED = {
   'frame-src': `https://www.tiktok.com`,
   'script-src': `https://www.tiktok.com`,
 } satisfies ContentSecurityPolicyConfig;
+
+/**
+ * CSP directives for tiktok pixel
+ *
+ * @see https://business-api.tiktok.com/portal/docs?id=1739585704427522
+ */
+export const CSP_PRESET_TIKTOK_PIXEL = {
+  'connect-src': `https://analytics.tiktok.com https://analytics-ipv6.tiktokw.us https://ads.tiktok.com`,
+  'frame-src': `'self' bytedance: sslocal:`,
+  'img-src': `https://analytics.tiktok.com https://analytics-ipv6.tiktokw.us https://ads.tiktok.com`,
+  'script-src': `https://analytics.tiktok.com https://analytics-ipv6.tiktokw.us https://ads.tiktok.com`,
+} satisfies ContentSecurityPolicyConfig;
