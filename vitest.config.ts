bb9f2a3dc1d61934eed171e7a468cbdb2614d264
meteorlxy <meteor.lxy@foxmail.com>
2026-01-15 19:10:07 +0800

    test: update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      all: true,
       include: ['src/**'],
       provider: 'istanbul',
       reporter: ['clover', 'json', 'lcov', 'text'],
